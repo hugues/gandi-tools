e713b1e48644055b055492dfae6f59e2b1f10426
Hugues Hiegel <hugues@hiegel.fr>
2016-01-19 17:44:18 +0100

    postfix/syncFwdMails.py: full rewrite
    much more quickier. Processes all fields in python,
    then do the calls in once.

--- postfix/syncFwdMails.py
+++ postfix/syncFwdMails.py
@@ -5,47 +5,61 @@ import xmlrpclib
 api = xmlrpclib.ServerProxy('https://rpc.gandi.net/xmlrpc/')
 apikey = open("/etc/postfix/Gandi.ApiKey").read().strip()
 
-virtual="/etc/postfix/virtual"
-
-domains={}
+domains=[]
 for entry in api.domain.list(apikey):
-	print "Found " + entry['fqdn']
-	domains[entry['fqdn']]=None
+	print "* Found " + entry['fqdn']
+	domains.append(entry['fqdn'])
 
-for line in open(virtual):
-	if "#" not in line and "DOMAIN" not in line and line != "\n":
-		words=line.split()
-		words[:1]=words[0].split("@")
-		try:
-			alias, domain, email= words[0],words[1],words[2]
+gandi={}
+for domain in domains:
+	for entry in api.domain.forward.list(apikey, domain):
+		gandi[entry['source']+"@"+domain] = entry['destinations']
 
-			if domain not in domains.keys():
-				print "[ ERROR ] " + alias + "@" + domain + " " + email + ": «" + domain + "» not managed"
-				continue
+virtual={}
+for line in open('/etc/postfix/virtual', 'ro'):
+	if "DOMAIN" in line:
+		domain=line.split()[0].strip('#\n')
+	else:
+		if line != "\n":
+			if "#" in line:
+				if "GANDI" in line:
+					source, destinations=line.split()[0:2]
+					source=source.lstrip('#')
+				else:
+					continue
+			else:
+				source, destinations=line.split()
 
-			elif domains[domain] is None:
-				domains[domain] = api.domain.forward.list(apikey, domain)
+			if "@" not in source:
+				source=source+"@"+domain
+			virtual[source]=destinations.split(',')
 
-			found=False
-			for entry in domains[domain][:]:
-				if alias in entry.values():
-					found=True
-					if email not in entry['destinations']:
-						try:
-							api.domain.forward.update(apikey, domain, alias, {'destinations':email.split(',')})
-							print "[UPDATED] " + alias + "@" + domain + "\r\t\t\t\t\t→ " + email + " (" + ','.join(entry['destinations']) + ")"
-						except:
-							print "[ ERROR ] " + alias + "@" + domain + "\r\t\t\t\t\t→ " + email
-					else:
-						print "          " + alias + "@" + domain + "\r\t\t\t\t\t→ " + email
+DELETE=set.difference(set(gandi), set(virtual))
+CREATE=set.difference(set(virtual), set(gandi))
 
-			if not found:
-				try:
-					api.domain.forward.create(apikey, domain, alias, {'destinations':email.split(',')})
-					print "[CREATED] " + alias + "@" + domain + "\r\t\t\t\t\t→ " + email
-				except:
-					print "[ ERROR ] " + alias + "@" + domain + "\r\t\t\t\t\t→ " + email
+for email in set.union(set(gandi), set(virtual)):
+	alias, domain=email.split('@')
+	if domain not in domains:
+		print "[ ERROR ] " + email + ": «" + domain + "» not managed."
+		continue
 
-		except:
-			print "[virtual] bad entry: " + line
+	elif email in CREATE:
+		# Add entry...
+		api.domain.forward.create(apikey, domain, alias, {'destinations':virtual[email]})
+		print "[CREATED] " + alias + "@" + domain + "\r\t\t\t\t\t → " + ','.join(virtual[email])
+		continue
+
+	elif email in DELETE:
+		# Remove entry...
+		api.domain.forward.delete(apikey, domain, alias)
+		print "[REMOVED] " + alias + "@" + domain + "\r\t\t\t\t\t (" + ','.join(gandi[email]) + ")"
+		continue
+
+	elif virtual[email] != gandi[email]:
+		# Update entry...
+		api.domain.forward.update(apikey, domain, alias, {'destinations':virtual[email]})
+		print "[UPDATED] " + alias + "@" + domain + "\r\t\t\t\t\t→ " + ','.join(virtual[email]) + " (" + ','.join(gandi[email]) + ")"
+
+	else:
+		print "          " + alias + "@" + domain + "\r\t\t\t\t\t→ " + ','.join(virtual[email])
 
